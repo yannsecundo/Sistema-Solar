7d4666678a3858a224e509aa7a8cb9878771a467
yannsecundo <yannsecundo34@gmail.com>
2022-06-21 17:22:18 -0400

    requisito 1 feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <>
-        <header>Sistema Solar</header>
-        <h1> Meu h1 </h1>
-
-      </>
+      <header>
+        <h1> Sistema Solar </h1>
+      </header>
     );
   }
 }
