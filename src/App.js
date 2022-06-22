82a332ca3f9697a6ca01e7326f9896b46f702dec
yannsecundo <yannsecundo34@gmail.com>
2022-06-22 08:31:23 -0400

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header titulo="Sistema Solar" />
-        <SolarSystem />
+        <SolarSystem>
+          <Title />
+        </SolarSystem>
       </>
     );
   }
